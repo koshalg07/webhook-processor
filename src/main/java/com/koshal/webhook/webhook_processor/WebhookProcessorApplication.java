3c492695db4a3518c21135d571630036d6db8bfd
koshalg07 <koshalg0777@gmail.com>
2025-10-28 17:04:17 +0530

    initialised project structure and base configs for db

---
+++ src/main/java/com/koshal/webhook/webhook_processor/WebhookProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.koshal.webhook.webhook_processor;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookProcessorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookProcessorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/koshal/webhook/webhook_processor/WebhookProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koshal.webhook.webhook_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
